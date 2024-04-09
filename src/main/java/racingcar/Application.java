c082b80f9e84bd186ee1993ca51093b069d0b92d
SCALAR127 <scalar127@gmail.com>
2024-04-10 00:39:17 +0900

    횟수입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,15 +2,17 @@ package racingcar;
 import java.util.ArrayList;
 public class Application{
     public static void main(String[] args) {
-        String input = System.console().readLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        String [] carNames = input.split("\\s*, \\s*");
+        String inputName = System.console().readLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String [] carNames = inputName.split("\\s*, \\s*");
+
+        String inputStr = System.console().readLine("시도할 회수를 입력하세요");
+        int inputCount = Integer.parseInt(inputStr);
 
         checkError.checkCarName(carNames);
 
 
     }
 }
-
 class checkError{
     public static void checkCarName(String [] carNames) {
         for (int i = 0; i < carNames.length; i++) {
