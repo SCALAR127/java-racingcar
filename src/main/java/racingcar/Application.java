def6b6bbc7cece9f80a6f3bbc13e2ba76fec28ec
SCALAR127 <scalar127@gmail.com>
2024-04-09 19:47:13 +0900

    예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
-
-public class Application {
+import java.util.ArrayList;
+public class Application{
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = System.console().readLine("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String [] carNames = input.split("\\s*, \\s*");
+
+        checkError.checkCarName(carNames);
+
+
     }
 }
+
+class checkError{
+    public static void checkCarName(String [] carNames) {
+        for (int i = 0; i < carNames.length; i++) {
+            if (carNames[i].length() > 5) {
+                System.out.println("IllegalArgumentException");
+                return;
+            }
+        }
+    }
+}
+
